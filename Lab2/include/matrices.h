c7cfca96891c932e582e6825333589cb736c5470
Fernando Zanutto <ferzanutto1999@gmail.com>
2022-03-05 18:27:13 -0300

    Matriz de translação

--- Lab2/include/matrices.h
+++ Lab2/include/matrices.h
@@ -62,12 +62,10 @@ glm::mat4 Matrix_Identity()
 glm::mat4 Matrix_Translate(float tx, float ty, float tz)
 {
     return Matrix(
-        // PREENCHA AQUI A MATRIZ DE TRANSLAÇÃO (3D) EM COORD. HOMOGÊNEAS
-        // UTILIZANDO OS PARÂMETROS tx, ty e tz
-        0.0f , 0.0f , 0.0f , 0.0f ,  // LINHA 1
-        0.0f , 0.0f , 0.0f , 0.0f ,  // LINHA 2
-        0.0f , 0.0f , 0.0f , 0.0f ,  // LINHA 3
-        0.0f , 0.0f , 0.0f , 0.0f    // LINHA 4
+        1.0f , 0.0f , 0.0f , tx ,  // LINHA 1
+        0.0f , 1.0f , 0.0f , ty ,  // LINHA 2
+        0.0f , 0.0f , 1.0f , tz ,  // LINHA 3
+        0.0f , 0.0f , 0.0f , 1.0f    // LINHA 4
     );
 }
 
